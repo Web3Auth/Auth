19c11bec8721bf16aadba109fa52bca3c9f43a00
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-19 15:28:27 +0700

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require("path");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
-const generateLibraryName = (pkgName) => {
+function generateLibraryName(pkgName) {
   return pkgName.charAt(0).toUpperCase() + pkgName.slice(1);
-};
+}
 
 // loaders execute right to left
 
