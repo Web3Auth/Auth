bb9b80274b682543c4de3274e3817acc22ed8576
Leonard Tan <tetratorus@gmail.com>
2021-03-25 15:05:50 +0800

    Update types

--- packages/utils/types/src/utils.d.ts
+++ packages/utils/types/src/utils.d.ts
@@ -1,6 +1,6 @@
 import keccakLib from "keccak";
 export declare const keccak: typeof keccakLib;
 export declare type BufferEncoding = "ascii" | "utf8" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex";
-export { default as base64url } from "base64url";
+export declare const base64url: import("base64url").Base64Url;
 export declare function base64toJSON(b64str: string): Record<string, unknown>;
 export declare function jsonToBase64(json: Record<string, unknown>): string;
