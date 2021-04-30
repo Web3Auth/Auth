9f5cc4083f9d838e026abccd10bfd2f9d868b03e
Leonard Tan <tetratorus@gmail.com>
2021-04-30 22:36:12 +0800

    added safe atob and btoa

--- packages/utils/types/src/utils.d.ts
+++ packages/utils/types/src/utils.d.ts
@@ -1,4 +1,6 @@
 import keccakLib from "keccak";
+export declare function safebtoa(str: string): string;
+export declare function safeatob(str: string): string;
 export declare const keccak: typeof keccakLib;
 export declare type BufferEncoding = "ascii" | "utf8" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex";
 export declare const base64url: import("base64url").Base64Url;
