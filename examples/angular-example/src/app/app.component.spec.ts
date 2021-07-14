367364ac195ca87f2c089b6859fa8d46dc5b06ca
chaitanyapotti <chaitanya.potti@gmail.com>
2021-07-14 15:36:43 +0800

    more lint

--- examples/angular-example/src/app/app.component.spec.ts
+++ examples/angular-example/src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -19,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-example');
+    expect(app.title).toEqual("angular-example");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-example app is running!');
+    expect(compiled.querySelector(".content span").textContent).toContain("angular-example app is running!");
   });
 });
--- examples/angular-example/src/polyfills.ts
+++ examples/angular-example/src/polyfills.ts
@@ -57,8 +57,8 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
-import { Buffer } from 'buffer';
+import "zone.js"; // Included with Angular CLI.
+import { Buffer } from "buffer";
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
