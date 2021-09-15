c5c7d0a465dfe0aec9a3d48e9dd17a0c6fe59ae0
chaitanyapotti <chaitanya.potti@gmail.com>
2021-09-15 16:29:57 +0800

    Make domain optional

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -103,7 +103,7 @@ export interface ExtraLoginOptions extends BaseLoginOptions {
    * `'example.eu.auth0.com'` or , `'example.mycompany.com'`
    * (when using [custom domains](https://auth0.com/docs/custom-domains))
    */
-  domain: string;
+  domain?: string;
   /**
    * The Client ID found on your Application settings page
    */
