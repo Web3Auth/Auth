a8ad276edd6a156322e161a9f8fc4b822e15328e
chaitanyapotti <chaitanya.potti@gmail.com>
2021-10-13 22:51:35 +0800

    Fix filename

--- packages/wrapper/rollup.config.js
+++ packages/wrapper/rollup.config.js
@@ -3,7 +3,7 @@ import path from "path";
 import generateRollupConfig from "../../rollup.config";
 import pkg from "./package.json";
 
-const pkgName = pkg.name.split("/")[1];
+const pkgName = pkg.name.split("/")[1] || pkg.name.split("/")[0];
 
 const currentPath = path.resolve(".");
 
