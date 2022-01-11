85855ac9df4ee17e09782c2121898371f1affd87
chaitanyapotti <chaitanya.potti@gmail.com>
2022-01-11 20:38:23 +0800

    working login + vue upgrade

--- examples/vue-example/vue.config.js
+++ examples/vue-example/vue.config.js
@@ -1,7 +1,23 @@
+const path = require("path");
+const { ProvidePlugin } = require("webpack");
+
 module.exports = {
-  devServer: {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
+  transpileDependencies: true,
+  configureWebpack: (config) => {
+    config.resolve.fallback = {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      assert: require.resolve("assert"),
+      // os: require.resolve("os-browserify/browser"),
+      // https: require.resolve("https-browserify"),
+      // http: require.resolve("stream-http"),
+    };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "bn.js": path.resolve(__dirname, "node_modules/bn.js"),
+      lodash: path.resolve(__dirname, "node_modules/lodash"),
+    };
+    config.plugins.push(new ProvidePlugin({ Buffer: ["buffer", "Buffer"] }));
+    config.plugins.push(new ProvidePlugin({ process: ["process/browser"] }));
   },
 };
