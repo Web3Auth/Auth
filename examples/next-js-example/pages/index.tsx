1da30566114c2de4ff80bb43cc54a09606bbf5ad
chaitanyapotti <chaitanya.potti@gmail.com>
2022-02-14 18:15:45 +0800

    Fix nextjs example

--- examples/next-js-example/pages/index.tsx
+++ examples/next-js-example/pages/index.tsx
@@ -1,31 +1,31 @@
-import type OpenLogin from "openlogin";
+import OpenLogin from "openlogin";
 import { useEffect, useState } from "react";
 import * as bs58 from "bs58";
 import { getED25519Key } from "@toruslabs/openlogin-ed25519";
 
-let openlogin: OpenLogin;
 const YOUR_PROJECT_ID = "BOUSb58ft1liq2tSVGafkYohnNPgnl__vAlYSk3JnpfW281kApYsw30BG1-nGpmy8wK-gT3dHw2D_xRXpTEdDBE";
 function App() {
   const [loading, setLoading] = useState(false);
+  const [openLogin, setOpenLogin] = useState<OpenLogin | null>(null);
 
   useEffect(() => {
     setLoading(true);
-    async function initializeOpenlogin() {
-      const OpenloginSdk = (await import("openlogin")).default;
-      openlogin = new OpenloginSdk({
+    async function initializeOpenLogin() {
+      const openLoginInstance = new OpenLogin({
         // your clientId aka projectId , get it from https://developer.tor.us
         // clientId is not required for localhost, you can set it to any string
         // for development
         clientId: YOUR_PROJECT_ID,
         network: "testnet",
       });
-      await openlogin.init();
-      if (openlogin.privKey) {
-        await printUserInfo()
+      await openLoginInstance.init();
+      setOpenLogin(openLoginInstance);
+      if (openLoginInstance.privKey) {
+        await printUserInfo();
       }
       setLoading(false);
     }
-    initializeOpenlogin();
+    initializeOpenLogin();
   }, []);
 
   const printToConsole = (...args: unknown[]): void => {
@@ -35,9 +35,10 @@ function App() {
     }
   };
   async function login() {
+    if (!openLogin) return;
     setLoading(true);
     try {
-      const privKey = await openlogin.login({
+      const privKey = await openLogin.login({
         // pass empty string '' as loginProvider to open default torus modal
         // with all default supported login providers or you can pass specific
         // login provider from available list to set as default.
@@ -45,16 +46,6 @@ function App() {
         loginProvider: "",
         redirectUrl: `${window.origin}`,
         relogin: true,
-        // setting it true will force user to use touchid/faceid (if available on device)
-        // while doing login again
-        fastLogin: false,
-
-        // setting skipTKey to true will display a button to user to skip
-        // openlogin security while login.
-        // But caveat here is that user will be get different keys if user is skipping tkey
-        // so use this option with care in your app or make sure user knows about this.
-        skipTKey: false,
-
         // you can pass standard oauth parameter in extralogin options
         // for ex: in case of passwordless login, you have to pass user's email as login_hint
         // and your app domain.
@@ -75,19 +66,22 @@ function App() {
   }
 
   const printUserInfo = async () => {
-    const userInfo = await openlogin.getUserInfo();
+    if (!openLogin) return;
+    const userInfo = await openLogin.getUserInfo();
     printToConsole(userInfo);
   };
 
   const getEd25519Key = async () => {
-    const { sk } = getED25519Key(openlogin.privKey);
+    if (!openLogin) return;
+    const { sk } = getED25519Key(openLogin.privKey);
     const base58Key = bs58.encode(sk);
     printToConsole(base58Key);
   };
 
   const logout = async () => {
+    if (!openLogin) return;
     setLoading(true);
-    await openlogin.logout({});
+    await openLogin.logout({});
     setLoading(false);
   };
 
@@ -110,7 +104,7 @@ function App() {
         </div>
       ) : (
         <div className="App">
-          {!openlogin?.privKey ? (
+          {!openLogin?.privKey ? (
             <div>
               <h3>Login With Openlogin</h3>
               <button onClick={login}>login</button>
@@ -120,11 +114,11 @@ function App() {
               <section>
                 <div>
                   Private key:
-                  <i>{openlogin.privKey}</i>
+                  <i>{openLogin.privKey}</i>
                   <button onClick={() => logout()}>Logout</button>
                 </div>
                 <div>
-                  <button onClick={getUserInfo}>Get User Info</button>
+                  <button onClick={printUserInfo}>Get User Info</button>
                   <button onClick={getEd25519Key}>Get Ed25519Key </button>
 
                   <div id="console" style={{ whiteSpace: "pre-line" }}>
