98752f0284c8ff0c6af3f7b4e313e1f1d58398ba
Lionell Briones <llenoil@gmail.com>
2022-05-23 18:21:57 +0800

    added openlogin without whitelabel

--- examples/vue-example/src/App.vue
+++ examples/vue-example/src/App.vue
@@ -64,7 +64,7 @@ const openlogin = new OpenLogin({
     url: "http://localhost:8080",
     logoDark: "https://images.web3auth.io/example-hello.svg", // dark logo for light background
     logoLight: "https://images.web3auth.io/example-hello-light.svg", // light logo for dark background
-    dark: false,
+    dark: true,
     theme: {
       primary: "#FF9900",
     },
