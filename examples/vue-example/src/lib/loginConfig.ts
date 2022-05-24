fbf86c49022caabce973af5f97875fa165a6f0cc
Lionell Briones <llenoil@gmail.com>
2022-05-24 13:26:50 +0800

    updated login config

--- examples/vue-example/src/lib/loginConfig.ts
+++ examples/vue-example/src/lib/loginConfig.ts
@@ -2,8 +2,8 @@ import { LoginConfig } from "@toruslabs/openlogin-jrpc";
 import { LOGIN_PROVIDER } from "openlogin";
 
 export default {
-  email_password: {
-    loginProvider: "email_password",
+  email_passwordless: {
+    loginProvider: "email_passwordless",
     name: "Binance Login",
     typeOfLogin: LOGIN_PROVIDER.JWT,
     description: "Login with Auth0",
@@ -14,9 +14,9 @@ export default {
     showOnMobile: true,
     mainOption: true,
     verifierSubIdentifier: "",
-    // logoDark: "https://images.web3auth.io/example-login-hello-dark.svg",
-    // logoLight: "https://images.web3auth.io/example-login-hello-light.svg",
-    // logoHover: "https://images.web3auth.io/example-login-hello-hover.svg",
+    logoDark: "https://images.web3auth.io/example-login-hello-dark.svg",
+    logoLight: "https://images.web3auth.io/example-login-hello-light.svg",
+    logoHover: "https://images.web3auth.io/example-login-hello-hover.svg",
     jwtParameters: {
       domain: "https://dev-zraq1p5o.us.auth0.com",
       connection: "Username-Password-Authentication",
