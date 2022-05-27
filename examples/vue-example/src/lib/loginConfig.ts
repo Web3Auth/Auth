d005ae54b2be6d0e1dcc4ac6b4316d3f1b5b0f7f
chaitanyapotti <chaitanya.potti@gmail.com>
2022-05-27 16:44:20 +0800

    comment passwordless

--- examples/vue-example/src/lib/loginConfig.ts
+++ examples/vue-example/src/lib/loginConfig.ts
@@ -2,26 +2,26 @@ import { LoginConfig } from "@toruslabs/openlogin-jrpc";
 import { LOGIN_PROVIDER } from "openlogin";
 
 export default {
-  email_passwordless: {
-    loginProvider: "email_passwordless",
-    name: "Binance Login",
-    typeOfLogin: LOGIN_PROVIDER.JWT,
-    description: "Login with Auth0",
-    verifier: "lioneell-auth0-email-password",
-    clientId: "MfPdpVU82zbowrP1zefQg7mCCdXzENTG",
-    showOnModal: true,
-    showOnDesktop: true,
-    showOnMobile: true,
-    mainOption: true,
-    verifierSubIdentifier: "",
-    logoDark: "https://images.web3auth.io/example-login-hello-dark.svg",
-    logoLight: "https://images.web3auth.io/example-login-hello-light.svg",
-    logoHover: "https://images.web3auth.io/example-login-hello-hover.svg",
-    jwtParameters: {
-      domain: "https://dev-zraq1p5o.us.auth0.com",
-      connection: "Username-Password-Authentication",
-    },
-  } as LoginConfig[keyof LoginConfig],
+  // email_passwordless: {
+  //   loginProvider: "email_passwordless",
+  //   name: "Binance Login",
+  //   typeOfLogin: LOGIN_PROVIDER.JWT,
+  //   description: "Login with Auth0",
+  //   verifier: "lioneell-auth0-email-password",
+  //   clientId: "MfPdpVU82zbowrP1zefQg7mCCdXzENTG",
+  //   showOnModal: true,
+  //   showOnDesktop: true,
+  //   showOnMobile: true,
+  //   mainOption: true,
+  //   verifierSubIdentifier: "",
+  //   logoDark: "https://images.web3auth.io/example-login-hello-dark.svg",
+  //   logoLight: "https://images.web3auth.io/example-login-hello-light.svg",
+  //   logoHover: "https://images.web3auth.io/example-login-hello-hover.svg",
+  //   jwtParameters: {
+  //     domain: "https://dev-zraq1p5o.us.auth0.com",
+  //     connection: "Username-Password-Authentication",
+  //   },
+  // } as LoginConfig[keyof LoginConfig],
   google: {
     loginProvider: "google",
     name: "Google Login",
