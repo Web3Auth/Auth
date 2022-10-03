4e18c63435f31e62b26064dca637b4983105d521
chaitanyapotti <chaitanya.potti@gmail.com>
2022-10-03 11:30:59 +0800

    Fix window closing return command

--- packages/openlogin/src/utils.ts
+++ packages/openlogin/src/utils.ts
@@ -81,7 +81,10 @@ export function getHashQueryParams(replaceUrl = false): Record<string, string> {
 
 export function awaitReq<T>(id: string, windowRef: Window): Promise<T> {
   return new Promise((resolve, reject) => {
-    if (!windowRef) reject(new Error("Unable to open window"));
+    if (!windowRef) {
+      reject(new Error("Unable to open window"));
+      return;
+    }
     let closedByHandler = false;
     const closedMonitor = setInterval(() => {
       if (!closedByHandler && windowRef.closed) {
