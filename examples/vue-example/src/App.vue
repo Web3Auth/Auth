0db7abc601a939e5205e5da0d3ace0ddc6c753af
Ganesh Patra <ganeshpatra@Ganeshs-MacBook-Pro.local>
2023-04-13 16:34:15 +0530

    Openlogin revamp ui

--- examples/vue-example/src/App.vue
+++ examples/vue-example/src/App.vue
@@ -1,86 +1,59 @@
 <template>
-  <div id="app">
-    <div v-if="loading">
-      <div class="loader-wrap">
-        <h1>....loading</h1>
+  <div>
+    <!-- Loader -->
+    <div class="loader-container" v-if="loading">Loading...</div>
+    <!-- Login -->
+    <div class="login-container" v-if="!privKey">
+      <h1 class="login-heading">demo-openlogin.web3auth.io</h1>
+      <h3 class="login-subheading">Login in with Openlogin</h3>
+      <div class="login-btn">
+        <button class="btn" @click="login">Login</button>
+        <button class="btn" @click="loginWithoutWhitelabel">Login with WhiteLabel</button>
       </div>
     </div>
-    <div>
-      <!-- <div v-if="!privKey && !loading">
-        <h3>Login With Openlogin</h3>
-        <button @click="login">login</button>
-        <button @click="loginWithoutWhitelabel">login without whitelabel</button>
-      </div> -->
-      <div class="grid justify-center pt-20 text-center" v-if="!privKey && !loading">
-        <h7 class="text-3xl font-bold">demo-openlogin.web3auth.io</h7>
-        <h6 class="pb-10 font-semibold text-[#595857]">Login With Openlogin</h6>
+    <!-- Dashboard -->
+    <div v-else class="dashboard-container">
+      <!-- Dashboard Header -->
+      <div class="dashboard-header">
         <div>
-          <button @click="login" class="btn-login">Login</button>
-          <button @click="loginWithoutWhitelabel" class="btn-login">Login with Whitelabel</button>
+          <h1 class="dashboard-heading">demo-openlogin.web3auth.io</h1>
+          <p class="dashboard-subheading">Openlogin Private key : {{ privKey }}</p>
+        </div>
+        <div class="dashboard-action-container">
+          <p class="dashboard-chainid">Connect chainID : 0x3</p>
+          <button class="dashboard-action-logout" @click.stop="logout">
+            <img :src="require('@/assets/logout.svg')" alt="logout" height="18" width="18" />
+            Logout
+          </button>
         </div>
       </div>
-
-      <div v-if="privKey">
-        <div class="flex m-6 text-left box md:rows-span-2">
-          <div class="ml-6 overflow-hidden mt-7 text-ellipsis">
-            <h7 class="text-2xl font-semibold">demo-openlogin.web3auth.io</h7>
-            <h6 class="pb-8 overflow-hidden text-left text-ellipsis">Openlogin Private key : {{ privKey }}</h6>
+      <!-- Dashboard Action Container -->
+      <div class="dashboard-details-container">
+        <div class="dashboard-details-btn-container">
+          <p class="btn-label">User info</p>
+          <div class="flex-row bottom-gutter">
+            <button class="btn" @click="getUserInfo">Get user info</button>
+            <button class="btn" @click="getEd25519Key">Get Ed25519Key</button>
           </div>
-          <div class="ml-auto mt-7">
-            <span class="pr-32">Connected ChainId : {{ ethereumPrivateKeyProvider.state.chainId }}</span>
-            <button type="button" @click="logout" class="btn-logout">
-              <img src="@/assets/logout.svg" class="pl-0 pr-3" />
-              Logout
-            </button>
+          <p class="btn-label">Signing</p>
+          <div class="flex-row bottom-gutter">
+            <button class="btn" :disabled="!ethereumPrivateKeyProvider?.provider" @click="signMessage">Sign test Eth Message</button>
+            <button class="btn" :disabled="!ethereumPrivateKeyProvider?.provider" @click="latestBlock">Fetch latest block</button>
+          </div>
+          <div class="flex-row bottom-gutter">
+            <button class="btn" :disabled="!ethereumPrivateKeyProvider?.provider" @click="addChain">Add to rinkeby</button>
+            <button class="btn" :disabled="!ethereumPrivateKeyProvider?.provider" @click="switchChain">Switch to rinkeby</button>
+          </div>
+          <div class="flex-row bottom-gutter">
+            <button class="btn" :disabled="!ethereumPrivateKeyProvider?.provider" @click="signV1Message">Sign Typed data v1 test message</button>
           </div>
         </div>
-        <div class="grid grid-cols-5 m-6 gap-7 height-fit">
-          <div class="grid grid-cols-2 col-span-5 gap-2 p-4 text-left md:col-span-2 box">
-            <div class="col-span-2 text-left">
-              <div class="font-semibold">User Info</div>
-              <div class="grid grid-cols-2 gap-2">
-                <button class="btn" @click="getUserInfo">Get user info</button>
-                <button class="btn" @click="getEd25519Key">Get Ed25519Key</button>
-              </div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="font-semibold">Signing</div>
-              <div class="grid grid-cols-2 gap-2">
-                <button class="btn" @click="signMessage" :disabled="!ethereumPrivateKeyProvider.provider">Sign test Eth Message</button>
-                <button class="btn" @click="signV1Message" :disabled="!ethereumPrivateKeyProvider.provider">Sign Typed data v1 test message</button>
-                <button class="btn" @click="latestBlock" :disabled="!ethereumPrivateKeyProvider.provider">Fetch latest block</button>
-                <button class="btn" @click="addChain" :disabled="!ethereumPrivateKeyProvider.provider">Add Rinkeby Chain</button>
-                <button class="btn" @click="switchChain" :disabled="!ethereumPrivateKeyProvider.provider">Switch to rinkeby</button>
-              </div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="grid grid-cols-2 gap-2"></div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="grid grid-cols-2 gap-2"></div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="grid grid-cols-2 gap-2"></div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="grid grid-cols-2 gap-2"></div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="grid grid-cols-2 gap-2"></div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="grid grid-cols-2 gap-2"></div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="grid grid-cols-2 gap-2"></div>
-            </div>
-            <div class="col-span-2 text-left">
-              <div class="grid grid-cols-2 gap-2"></div>
-            </div>
-          </div>
-          <div class="box-grey" id="console">
-            <p style="white-space: pre-line"></p>
-            <div><button class="clear-button" @click="clearUiconsole">Clear console</button></div>
+        <!-- Dashboard Console Container -->
+        <div class="dashboard-details-console-container" id="console">
+          <h1 class="console-heading"></h1>
+          <pre class="console-container"></pre>
+          <div class="clear-console-btn">
+            <button class="btn console-btn" @click="clearConsole">Clear console</button>
           </div>
         </div>
       </div>
@@ -89,28 +62,29 @@
 </template>
 
 <script lang="ts">
-/* eslint-disable */
 import { getED25519Key } from "@toruslabs/openlogin-ed25519";
-import { DEFAULT_INFURA_ID } from "@web3auth/base";
+// TODO
+// import DEFAULT_INFURA_ID from "@web3auth/base";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import * as bs58 from "bs58";
-import Vue from "vue";
+import { defineComponent } from "vue";
 
 import * as ethWeb3 from "./lib/ethWeb3";
 import { getOpenLoginInstance } from "./lib/openlogin";
 import whitelabel from "./lib/whitelabel";
 
-export default Vue.extend({
+export default defineComponent({
   name: "App",
   data() {
     return {
       loading: false,
       privKey: "",
       ethereumPrivateKeyProvider: null as EthereumPrivateKeyProvider | null,
+      // TODO
+      DEFAULT_INFURA_ID: "776218ac4734478c90191dde8cae483c",
     };
   },
   async mounted() {
-    this.loading = true;
     const openlogin = getOpenLoginInstance();
     await openlogin.init();
     if (openlogin.privKey) {
@@ -123,12 +97,13 @@ export default Vue.extend({
     async login() {
       try {
         this.loading = true;
+        console.log("calles");
         const openlogin = getOpenLoginInstance(whitelabel);
         // in popup mode (with third party cookies available) or if user is already logged in this function will
         // return priv key , in redirect mode or if third party cookies are blocked then priv key be injected to
         // sdk instance after calling init on redirect url page.
         const privKey = await openlogin.login({
-          loginProvider: 'google',
+          loginProvider: "google",
           mfaLevel: "optional",
           // pass empty string '' as loginProvider to open default torus modal
           // with all default supported login providers or you can pass specific
@@ -152,12 +127,6 @@ export default Vue.extend({
         console.error(error);
       } finally {
         this.loading = false;
-      }
-    },
-    async clearUiconsole() {
-      const el = document.querySelector("#console>p");
-      if (el) {
-        el.innerHTML = "";
       }
     },
 
@@ -186,7 +155,7 @@ export default Vue.extend({
         config: {
           chainConfig: {
             chainId: "0x3",
-            rpcTarget: `https://ropsten.infura.io/v3/${DEFAULT_INFURA_ID}`,
+            rpcTarget: `https://ropsten.infura.io/v3/${this.DEFAULT_INFURA_ID}`,
             displayName: "ropsten",
             blockExplorer: "https://ropsten.etherscan.io/",
             ticker: "ETH",
@@ -200,31 +169,27 @@ export default Vue.extend({
     async getUserInfo() {
       const openlogin = getOpenLoginInstance();
       const userInfo = await openlogin.getUserInfo();
-      this.printToConsole(userInfo);
+      this.printToConsole("User Info", userInfo);
     },
 
-    getEd25519Key() {
-      const openlogin = getOpenLoginInstance();
-      const { sk } = getED25519Key(openlogin.privKey);
-      const base58Key = bs58.encode(sk);
-      this.printToConsole(base58Key);
-    },
     async signMessage() {
       if (!this.ethereumPrivateKeyProvider?.provider) throw new Error("provider not set");
       const signedMessage = await ethWeb3.signEthMessage(this.ethereumPrivateKeyProvider.provider);
-      this.printToConsole("signedMessage", signedMessage);
+      this.printToConsole("Signed Message", signedMessage);
     },
+
     async signV1Message() {
       if (!this.ethereumPrivateKeyProvider?.provider) throw new Error("provider not set");
       const signedMessage = await ethWeb3.signTypedData_v1(this.ethereumPrivateKeyProvider.provider);
-      this.printToConsole("signedMessage", signedMessage);
+      this.printToConsole("Signed Message", signedMessage);
     },
+
     async latestBlock() {
       if (!this.ethereumPrivateKeyProvider?.provider) throw new Error("provider not set");
-
       const block = await ethWeb3.fetchLatestBlock(this.ethereumPrivateKeyProvider.provider);
-      this.printToConsole("latest block", block);
+      this.printToConsole("Latest block", block);
     },
+
     async switchChain() {
       if (!this.ethereumPrivateKeyProvider?.provider) throw new Error("provider not set");
       try {
@@ -232,10 +197,10 @@ export default Vue.extend({
           method: "wallet_switchEthereumChain",
           params: [{ chainId: "0x4" }],
         });
-        this.printToConsole("switchedChain", this.ethereumPrivateKeyProvider.state, this.ethereumPrivateKeyProvider.config);
+        this.printToConsole("Switched Chain", { ...this.ethereumPrivateKeyProvider.state, ...this.ethereumPrivateKeyProvider.config });
       } catch (error) {
         console.log("error while switching chain", error);
-        this.printToConsole("switchedChain error", error);
+        this.printToConsole("Switched Chain Error", error);
       }
     },
 
@@ -253,15 +218,15 @@ export default Vue.extend({
                 symbol: "ETH",
                 decimals: 18,
               },
-              rpcUrls: [`https://rinkeby.infura.io/v3/${DEFAULT_INFURA_ID}`],
+              rpcUrls: [`https://rinkeby.infura.io/v3/${this.DEFAULT_INFURA_ID}`],
               blockExplorerUrls: [`https://rinkeby.etherscan.io/`],
             },
           ],
         });
-        this.printToConsole("added chain", this.ethereumPrivateKeyProvider.state, this.ethereumPrivateKeyProvider.config);
+        this.printToConsole("Added chain", { ...this.ethereumPrivateKeyProvider.state, ...this.ethereumPrivateKeyProvider.config });
       } catch (error) {
         console.log("error while adding chain", error);
-        this.printToConsole("add chain error", error);
+        this.printToConsole("Add chain error", error);
       }
     },
 
@@ -271,91 +236,47 @@ export default Vue.extend({
       this.privKey = openlogin.privKey;
       this.ethereumPrivateKeyProvider = null;
     },
+
+    getEd25519Key() {
+      const openlogin = getOpenLoginInstance();
+      const { sk } = getED25519Key(openlogin.privKey);
+      const base58Key = bs58.encode(sk);
+      this.printToConsole("Ed25519Key", base58Key);
+    },
+
     printToConsole(...args: unknown[]) {
-      const el = document.querySelector("#console>p");
+      const el = document.querySelector("#console>pre");
+      const h1 = document.querySelector("#console>h1");
+      const consoleBtn = document.querySelector<HTMLElement>("#console>div.clear-console-btn");
+      if (h1) {
+        h1.innerHTML = args[0] as string;
+      }
       if (el) {
-        el.innerHTML = JSON.stringify(args || {}, null, 2);
+        el.innerHTML = JSON.stringify(args[1] || {}, null, 2);
+      }
+      if (consoleBtn) {
+        consoleBtn.style.display = "block";
+      }
+    },
+
+    clearConsole() {
+      const el = document.querySelector("#console>pre");
+      const h1 = document.querySelector("#console>h1");
+      const consoleBtn = document.querySelector<HTMLElement>("#console>div.clear-console-btn");
+      if (h1) {
+        h1.innerHTML = "";
+      }
+      if (el) {
+        el.innerHTML = "";
+      }
+      if (consoleBtn) {
+        consoleBtn.style.display = "none";
       }
     },
   },
 });
 </script>
 
-<style>
-.box {
-  @apply bg-white;
-  border: 1px solid #f3f3f4;
-  border-radius: 20px;
-  box-shadow: 4px 4px 20px rgba(46, 91, 255, 0.1);
-}
-
-.box-grey {
-  @apply col-span-5 md:col-span-3 overflow-hidden min-h-[400px] bg-[#f3f3f4] rounded-3xl relative;
-  border: 1px solid #f3f3f4;
-  box-shadow: 4px 4px 20px rgba(46, 91, 255, 0.1);
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-/* #app {
-  font-family: 'DM Sans';
-  font-style: normal;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-} */
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-/* button {
-  height: 25px;
-  margin: 5px;
-  background: none;
-  border-radius: 5px;
-  cursor: pointer;
-} */
-.btn-login {
-  @apply h-12 w-60 m-2 bg-white rounded-3xl font-[#6F717A] font-medium;
-  border: 1px solid #6f717a;
-}
-#console {
-  text-align: left;
-  overflow: auto;
-}
-#console > p {
-  @apply m-2;
-}
-.btn {
-  @apply h-11 w-full m-0 bg-white rounded-3xl text-[#6F717A] text-sm lg:text-base font-medium;
-  border: 1px solid #6f717a;
-}
-
-.btn-logout {
-  @apply h-12 w-32 bg-white rounded-3xl pl-6 m-2 text-sm inline-flex items-center;
-  border: 1px solid #f3f3f4;
-}
-.clear-button {
-  @apply absolute md:fixed right-8 bottom-2 md:right-8 md:bottom-12 w-28 h-7 bg-[#f3f3f4] rounded-md;
-  border: 1px solid #0f1222;
-}
-.height-fit {
-  @apply min-h-fit;
-  height: 75vh;
-}
+<style scoped>
+@import "./App.css";
 </style>
