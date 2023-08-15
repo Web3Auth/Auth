91993ea9606cbd225f9d0a8f7ffc9b3ef9ebbbb7
Archit <arch1995@gmail.com>
2023-08-15 17:57:30 +0530

    adds rollup

---
+++ packages/openlogin/rollup.config.js
@@ -0,0 +1,12 @@
+import replace from "@rollup/plugin-replace";
+
+import pkg from "./package.json";
+
+export default {
+  plugins: [
+    replace({
+      "process.env.OPENLOGIN_VERSION": `"${pkg.version}"`,
+      preventAssignment: true,
+    }),
+  ],
+};
