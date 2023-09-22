7e72b781066696715dd332216d44b1987b0d4b87
Archit <arch1995@gmail.com>
2023-09-22 12:22:54 +0800

    fix eth_sign

--- examples/vue-example/src/lib/ethWeb3.ts
+++ examples/vue-example/src/lib/ethWeb3.ts
@@ -5,7 +5,7 @@ export const signEthMessage = async (provider: SafeEventEmitterProvider): Promis
   const web3 = new BrowserProvider(provider as any);
   const accounts = await web3.listAccounts();
   // hex message
-  const message = "0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad";
+  const message = Buffer.from("47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad", "hex");
   const sign = await accounts[0]._legacySignMessage(message);
   return sign;
 };
