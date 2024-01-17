4030d8c3f5ee2c3f1724937a43384a773e8579cb
Naveed Elahi <statix231@hotmail.com>
2024-01-17 18:42:41 +0800

    create function to apply white label theme

--- packages/openlogin-utils/src/index.ts
+++ packages/openlogin-utils/src/index.ts
@@ -2,3 +2,5 @@ export * from "./browserStorage";
 export * from "./constants";
 export * from "./interfaces";
 export * from "./utils";
+// TODO: code split this to be importable separately
+export * from "./whitelabel";
