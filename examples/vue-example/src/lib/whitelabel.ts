f7f0f6dfb4b2830ef661d2a17378cbb2e133b841
Naveed Elahi <statix231@hotmail.com>
2024-01-18 22:37:39 +0800

    fix demo app passing older ver. whitelabel interface params
    should be `appName` not `name` in the latest versions

--- examples/vue-example/src/lib/whitelabel.ts
+++ examples/vue-example/src/lib/whitelabel.ts
@@ -1,7 +1,7 @@
 import { WhiteLabelData } from "@toruslabs/openlogin-utils";
 
-export default {
-  name: "HelloDemo",
+const whiteLabel: WhiteLabelData = {
+  appName: "HelloDemo",
   appUrl: "http://localhost:8080",
   logoDark: "https://images.web3auth.io/example-hello.svg", // dark logo for light background
   logoLight: "https://images.web3auth.io/example-hello-light.svg", // light logo for dark background
@@ -9,4 +9,6 @@ export default {
   theme: {
     primary: "#FF9900",
   },
-} as WhiteLabelData;
+};
+
+export default whiteLabel;
