b13bc9ff4df0d611d85ae44d33d7e958884297f5
hieu-w <hieu@tor.us>
2024-07-09 01:20:43 +0700

    Vue Demo: Setup Icon plugin

--- examples/vue-example/src/main.ts
+++ examples/vue-example/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import "./tailwind.css";
 import App from "./App.vue";
+import createIcons from "./plugins/iconPlugin";
 
-const app = createApp(App);
+const app = createApp(App as any);
+
+app.use(createIcons)
 
 app.mount("#app");
