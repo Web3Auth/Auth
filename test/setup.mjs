5bd8448955bcd83d20ff55c968bfcc22ed856c91
Archit <arch1995@gmail.com>
2024-07-29 23:34:44 +0530

    fixes tests + ci

--- test/setup.mjs
+++ test/setup.mjs
@@ -1,8 +1,4 @@
 import register from "@babel/register";
-import path from "path";
-import { register as tsRegister } from "ts-node";
-
-tsRegister({ project: path.resolve("tsconfig.json") });
 
 register({
   presets: [["@babel/env", { bugfixes: true }], "@babel/typescript"],
