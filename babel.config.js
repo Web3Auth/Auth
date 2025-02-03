42bbbae9840da992665278b48556612cc05d1873
hieu-w <hieu@tor.us>
2025-02-03 16:52:31 +0700

    Fix babel missing hasOwn transform

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,6 @@
-module.exports = require("@toruslabs/config/babel.config.js");
+const torusConfig = require("@toruslabs/config/babel.config.js");
+
+module.exports = {
+  ...torusConfig,
+  plugins: [...torusConfig.plugins, "babel-plugin-transform-object-hasown"],
+};
