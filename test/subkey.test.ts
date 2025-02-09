ccc1cb903654c71258a7b381e62439550fce4bf7
hieu-w <hieu@tor.us>
2025-02-10 02:31:56 +0700

    Migrate tests to vitest

--- test/subkey.test.ts
+++ test/subkey.test.ts
@@ -1,14 +1,14 @@
-import { deepStrictEqual } from "assert";
+import { describe, expect, it } from "vitest";
 
 import { subkey } from "../src/subkey";
 
 const PRIVATE_KEY = "f70fb5f5970b363879bc36f54d4fc0ad77863bfd059881159251f50f48863acc";
 const clientId = "BCtbnOamqh0cJFEUYA0NB5YkvBECZ3HLZsKfvSRBvew2EiiKW3UxpyQASSR0artjQkiUOCHeZ_ZeygXpYpxZjOs";
 
-describe("Sub Key Derivation", function () {
-  it("#should derive subkey correctly", async function () {
+describe("Sub Key Derivation", () => {
+  it("should derive subkey correctly", async () => {
     const privKey = PRIVATE_KEY;
     const subKeyDerived = subkey(privKey, Buffer.from(clientId, "base64"));
-    deepStrictEqual(subKeyDerived, "2a152b927d3b01ffa912c68050d795ac59e1e4672506cdc73a29d50aa1933405", "derived key should be correct");
+    expect(subKeyDerived).toBe("2a152b927d3b01ffa912c68050d795ac59e1e4672506cdc73a29d50aa1933405");
   });
 });
