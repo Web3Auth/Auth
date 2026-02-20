92887a641fb33e1947387d59882d0b1ee93741a0
Archit <arch1995@gmail.com>
2026-02-20 23:58:43 +0400

    stack upgrade

--- examples/vue-example/src/lib/ethWeb3.ts
+++ examples/vue-example/src/lib/ethWeb3.ts
@@ -1,23 +1,16 @@
-import { SafeEventEmitterProvider } from "@web3auth/base";
-import { BrowserProvider } from "ethers";
+import type { Signer } from "ethers";
 
-export const signEthMessage = async (provider: SafeEventEmitterProvider): Promise<string> => {
-  const web3 = new BrowserProvider(provider as any);
-  const accounts = await web3.listAccounts();
-  // hex message
-  const message = Buffer.from("47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad", "hex");
-  const sign = await accounts[0]._legacySignMessage(message);
-  return sign;
+export const personalSign = async (signer: Signer): Promise<string> => {
+  const message = "Hello, world!";
+  return signer.signMessage(message);
 };
 
-export const signTypedData_v1 = async (provider: SafeEventEmitterProvider): Promise<any> => {
-  const web3 = new BrowserProvider(provider as any);
-  const accounts = await web3.listAccounts();
+export const signTypedData_v1 = async (signer: Signer): Promise<string> => {
   const domain = {
     name: "og-nft",
     version: "1",
     chainId: 1,
-    verifyingContract: "0x",
+    verifyingContract: "0x0000000000000000000000000000000000000000",
   };
   const types = {
     Nft: [
@@ -25,23 +18,8 @@ export const signTypedData_v1 = async (provider: SafeEventEmitterProvider): Prom
       { name: "price", type: "uint256" },
     ],
   };
-  return accounts[0].signTypedData(domain, types, {
+  return signer.signTypedData(domain, types, {
     URI: "",
     price: "1",
   });
-};
-
-export const ethSignMpcMessage = async (provider: SafeEventEmitterProvider) => {
-  const web3 = new BrowserProvider(provider as any);
-  const accounts = await web3.listAccounts();
-  // hex message
-  const message = "0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad";
-  const sign = await accounts[0].signMessage(message);
-  return sign;
-};
-
-export const fetchLatestBlock = async (provider: SafeEventEmitterProvider): Promise<any> => {
-  const web3 = new BrowserProvider(provider as any);
-  const block = await web3.getBlock("latest");
-  return block;
 };
