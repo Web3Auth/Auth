40925edfcb80d743d94ee6b43c00d1ffa2ab6609
lwin <lwin.kyaw@consensys.net>
2026-03-09 23:44:36 +0800

    fix: fixed PostMessageStream._postMessage override

--- src/jrpc/postMessageStream.ts
+++ src/jrpc/postMessageStream.ts
@@ -57,8 +57,8 @@ export class PostMessageStream extends BasePostMessageStream {
       if (typeof dataObj.data === "object") {
         const dataObjData = dataObj.data as Record<string, unknown>;
         if (Array.isArray(dataObjData.params) && dataObjData.params.length > 0) {
-          const dataObjDataParam = dataObjData.params[0] as Record<string, unknown>;
-          if (dataObjDataParam._origin) {
+          const dataObjDataParam = { ...dataObjData.params[0] } as Record<string, unknown>;
+          if (dataObjDataParam?._origin) {
             originConstraint = dataObjDataParam._origin as string;
           }
 
